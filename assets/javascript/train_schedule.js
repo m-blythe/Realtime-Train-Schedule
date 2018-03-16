8e6f208d2eb97a0229742f49a047f239c6eb6591
Blythe <m_blythe@yahoo.com>
2018-03-16 01:20:44 -0500

    update table on index.html

--- assets/javascript/train_schedule.js
+++ assets/javascript/train_schedule.js
@@ -26,23 +26,21 @@ $(document).ready(function () {
     });
 
     //when a new train is added, do this 
-    database.ref().on("child_added", function(childSnapshot) {
-        console.log("name "+childSnapshot.val().name);
-        console.log("dest "+childSnapshot.val().dest);
-        console.log("time "+childSnapshot.val().time);
-        console.log("freq "+childSnapshot.val().freq);
-        console.log("date added "+childSnapshot.val().dateAdded);
-       
+    database.ref().on("child_added", function (childSnapshot) {
+        console.log("name " + childSnapshot.val().name);
+        console.log("dest " + childSnapshot.val().dest);
+        console.log("time " + childSnapshot.val().time);
+        console.log("freq " + childSnapshot.val().freq);
+        console.log("date added " + childSnapshot.val().dateAdded);
+
         var tMinutesTillTrain = 0;
         var nextTrainConverted = 0;
-        
-        //add new train record into the main table
-        $("#trainTable > tbody").append("<tr><td>" + childSnapshot.val().name + "<tr><td>" + childSnapshot.val().dest 
-        + "<tr><td>" + "every " + childSnapshot.val().freq + " minutes" + "<tr><td>" + nextTrainConverted 
-        + "<tr><td>" + tMinutesTillTrain + "<tr><td>");
 
-    }, function(errorObject) {
+        var newRowContent = "<tr><td>" + childSnapshot.val().name + "</td><td>" + childSnapshot.val().dest + "</td><td>" + childSnapshot.val().freq + "</td><td>" + nextTrainConverted + "</td><td>" + tMinutesTillTrain + "</td></tr>"
+        $("#trainTable tbody").append(newRowContent);
+        
+    }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
-      });//closes the child added function
+    });//closes the child added function
 
 });//closes the document.ready
