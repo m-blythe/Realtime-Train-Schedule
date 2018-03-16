fa847d031d08d337e6a5f7caa83bf48b7987b61e
Blythe <m_blythe@yahoo.com>
2018-03-16 00:24:06 -0500

    updating the code to try to get records to show up in the table

--- assets/javascript/train_schedule.js
+++ assets/javascript/train_schedule.js
@@ -7,32 +7,42 @@ $(document).ready(function () {
     $("#submitButton").on("click", function () {
 
         var trainName = $("#trainNameInput").val().trim();
-        console.log(name);
+        //console.log(trainName,trainDest,trainTime,trainFreq);
         var trainDest = $("#destinationInput").val().trim();
         var trainTime = $("#timeInput").val().trim();
         var trainFreq = $("#frequencyInput").val().trim();
 
-
-        //create addTrain object
-        var addTrain = {
+        //add new train record to the database//
+        database.ref().push({
             name: trainName,
             dest: trainDest,
             time: trainTime,
-            freq: trainFreq
-        }
-        //add new train record to the database//
-        database.ref().push(addTrain);
+            freq: trainFreq,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
 
-alert("New train has been added!");
-
-//when a new train is added, do this 
-database.ref().on("child_added", function(childSnapshot, prevChildKey){
-
-    console.log(childSnapshot.val());
-});
-
+        alert("New train has been added!");
 
     });
 
+    //when a new train is added, do this 
+    database.ref().on("child_added", function(childSnapshot) {
+        console.log("name "+childSnapshot.val().name);
+        console.log("dest "+childSnapshot.val().dest);
+        console.log("time "+childSnapshot.val().time);
+        console.log("freq "+childSnapshot.val().freq);
+        console.log("date added "+childSnapshot.val().dateAdded);
+       
+        var tMinutesTillTrain = 0;
+        var nextTrainConverted = 0;
+        
+        //add new train record into the main table
+        $("#trainTable > tbody").append("<tr><td>" + childSnapshot.val().name + "<tr><td>" + childSnapshot.val().dest 
+        + "<tr><td>" + "every " + childSnapshot.val().freq + " minutes" + "<tr><td>" + nextTrainConverted 
+        + "<tr><td>" + tMinutesTillTrain + "<tr><td>");
 
-});
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });//closes the child added function
+
+});//closes the document.ready
