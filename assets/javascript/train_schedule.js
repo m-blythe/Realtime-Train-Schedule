ecfb25316916c4e2772cdd849b2c3f59be7fc41c
Blythe <m_blythe@yahoo.com>
2018-03-15 23:11:50 -0500

    add the calculation of determining arrival times and the URL for the firebase database

--- assets/javascript/train_schedule.js
+++ assets/javascript/train_schedule.js
@@ -1,64 +1,38 @@
-//javascript for realtime train schedule//
 $(document).ready(function () {
-    //confirm the file is properly linked//
-    //console.log("ready!");
-    var config = {
-        apiKey: "AIzaSyBT4xi7P-ghnrMMGiJI2xIOd0jXgBgSSgs",
-        authDomain: "realtime-train-schedule.firebaseapp.com",
-        databaseURL: "https://realtime-train-schedule.firebaseio.com",
-        projectId: "realtime-train-schedule",
-        storageBucket: "realtime-train-schedule.appspot.com",
-        messagingSenderId: "1003305421267"
-    };
 
-    firebase.initializeApp(config);
-
+    // Global Var
     var database = firebase.database();
 
-    //add new train, click on Submit button//
+    // Event listener for form
     $("#submitButton").on("click", function () {
+
         var trainName = $("#trainNameInput").val().trim();
-        var trainDestination = $("#destinationInput").val().trim();
-        var trainArrivalTime = $("#timeImput").val().trim();
-        var trainFrequency = $("#frequencyInput").val().trim();
+        console.log(name);
+        var trainDest = $("#destinationInput").val().trim();
+        var trainTime = $("#timeInput").val().trim();
+        var trainFreq = $("#frequencyInput").val().trim();
 
-        console.log(trainName);
-        console.log(trainDestination);
-        console.log(trainArrivalTime);
-        console.log(trainFrequency);
 
-        var addedTrain = {
+        //create addTrain object
+        var addTrain = {
             name: trainName,
-            destination:trainDestination,
-            arrival:trainArrivalTime,
-            frequency:trainFrequency,
+            dest: trainDest,
+            time: trainTime,
+            freq: trainFreq
         }
+        //add new train record to the database//
+        database.ref().push(addTrain);
 
-        trainData.push(addedTrain);
+alert("New train has been added!");
 
-        alert("New Train Added!");
+//when a new train is added, do this 
+database.ref().on("child_added", function(childSnapshot, prevChildKey){
 
-        //delete values from train form//
-        $("#trainNameInput").val("");
-        $("#destinationInput").val("");
-        $("#timeInput").val("");
-        $("#frequencyInput").val("");
-
-        return false;
-
-    });//closes the submit button function//
+    console.log(childSnapshot.val());
+});
 
 
+    });
 
 
-
-
-
-
-
-
-
-
-
-
-}) //closes the document ready
+});
